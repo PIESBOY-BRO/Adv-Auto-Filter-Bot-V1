64f7cd512916a00e71e4a9f168be4d1a0694b754
PIESBOY-BRO <89738311+PIESBOY-BRO@users.noreply.github.com>
2021-09-28 02:10:00 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -5,12 +5,16 @@
 class Translation(object):
     
     START_TEXT = """<b>Hey {}!!</b>
-<i>Am Just A Advance Auto Filter Bot....😉
+<i>𝐚𝐦 𝐭𝐡𝐞 𝐨𝐟𝐟𝐢𝐜𝐢𝐚𝐥 𝐀𝐮𝐭𝐨 𝐅𝐢𝐥𝐭𝐞𝐫 𝐁𝐨𝐭 𝐨𝐟 𝐭𝐡𝐞 "𝐌𝐨𝐯𝐢𝐞 𝐖𝐨𝐫𝐥𝐝" 𝐠𝐫𝐨𝐮𝐩....🤖
 
-Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
+↯ 𝐓𝐨 𝐠𝐞𝐭 𝐭𝐡𝐞 𝐦𝐨𝐯𝐢𝐞𝐬 𝐲𝐨𝐮 𝐧𝐞𝐞𝐝 ↯
 
-For More Details Click Help Button Below..
-@CrazyBotsz
+ ⓵: 𝐆𝐨 𝐭𝐨 𝐨𝐮𝐫 𝐠𝐫𝐨𝐮𝐩 𝐚𝐧𝐝 𝐫𝐞𝐪𝐮𝐞𝐬𝐭 𝐭𝐡𝐞 𝐧𝐚𝐦𝐞 𝐨𝐟 𝐭𝐡𝐞 𝐦𝐨𝐯𝐢𝐞 𝐭𝐡𝐞𝐫𝐞.
+ ⓶: 𝐓𝐡𝐞𝐧 𝐜𝐥𝐢𝐜𝐤 𝐨𝐧 𝐭𝐡𝐞 𝐥𝐢𝐧𝐤 𝐰𝐞 𝐩𝐫𝐨𝐯𝐢𝐝𝐞.
+ ⓷: 𝐓𝐡𝐞𝐧 𝐜𝐥𝐢𝐜𝐤 𝐨𝐧 𝐭𝐡𝐞 "𝐒𝐓𝐀𝐑𝐓" 𝐛𝐮𝐭𝐭𝐨𝐧.
+
+⇃𝗖𝗹𝗶𝗰𝗸 𝗼𝗻 𝘁𝗵𝗲 𝗯𝘂𝘁𝘁𝗼𝗻 𝗯𝗲𝗹𝗼𝘄 𝘁𝗼 𝗷𝗼𝗶𝗻 𝗼𝘂𝗿 𝗴𝗿𝗼𝘂𝗽𝘀⇂..⚡️
+@Movie_World_00
 </i>"""    
     
     HELP_TEXT = """
@@ -43,16 +47,16 @@ For More Details Click Help Button Below..
             
             -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
             
-@CrazyBotsz
+@Movie_World_00
 """
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/PIES_BOY">𝐏𝐈𝐄𝐒𝐁𝐎𝐘 ⎋</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
+<b>➥ Source Code</b> : <i><a href="https://github.com/PIESBOY-BRO/Adv-Auto-Filter-Bot-V1">Click Me</a></i>
 """
