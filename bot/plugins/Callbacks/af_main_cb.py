2d2b12f85b3dd5e1bcca948359eb0e4834640076
PIESBOY-BRO <89738311+PIESBOY-BRO@users.noreply.github.com>
2021-09-28 09:10:11 +0530

    Update af_main_cb.py

--- bot/plugins/Callbacks/af_main_cb.py
+++ bot/plugins/Callbacks/af_main_cb.py
@@ -78,7 +78,7 @@ async def cb_navg(bot, update: CallbackQuery):
 
     if ((index_val + 1 )== max_pages) or ((index_val + 1) == len(results)): # Max Pages
         temp_results.append([
-            InlineKeyboardButton("⏪ Back", callback_data=f"navigate({index_val}|back|{query})")
+            InlineKeyboardButton("⎗ Back", callback_data=f"navigate({index_val}|back|{query})")
         ])
 
     elif int(index_val) == 0:
@@ -86,7 +86,7 @@ async def cb_navg(bot, update: CallbackQuery):
 
     else:
         temp_results.append([
-            InlineKeyboardButton("⏪ Back", callback_data=f"navigate({index_val}|back|{query})"),
+            InlineKeyboardButton("⎗ Back", callback_data=f"navigate({index_val}|back|{query})"),
             InlineKeyboardButton("Next ⏩", callback_data=f"navigate({index_val}|next|{query})")
         ])
 
@@ -403,7 +403,7 @@ async def cb_set(bot, update: CallbackQuery):
         [
             InlineKeyboardButton
                 (
-                    "Back 🔙", callback_data=f"config({chat_id})"
+                    "Back ⎗", callback_data=f"config({chat_id})"
                 ),
             
             InlineKeyboardButton
@@ -428,10 +428,10 @@ async def callback_data(bot, update: CallbackQuery):
 
     if query_data == "start":
         buttons = [[
-            InlineKeyboardButton('My Dev 👨‍🔬', url='https://t.me/CrazyBotsz'),
-            InlineKeyboardButton('Source Code 🧾', url ='https://github.com/CrazyBotsz/Adv-Auto-Filter-Bot-V2')
+            InlineKeyboardButton('My Dev 👨‍🔬', url='https://t.me/MW_Links_00'),
+            InlineKeyboardButton('Source Code 🧾', url ='https://github.com/PIESBOY-BRO/Adv-Auto-Filter-Bot-V1')
         ],[
-            InlineKeyboardButton('Support 🛠', url='https://t.me/CrazyBotszGrp')
+            InlineKeyboardButton('Support 🛠', url='https://t.me/Movie_World_00')
         ],[
             InlineKeyboardButton('Help ⚙', callback_data="help")
         ]]
@@ -448,8 +448,8 @@ async def callback_data(bot, update: CallbackQuery):
 
     elif query_data == "help":
         buttons = [[
-            InlineKeyboardButton('Home ⚡', callback_data='start'),
-            InlineKeyboardButton('About 🚩', callback_data='about')
+            InlineKeyboardButton('Home ⎋', callback_data='start'),
+            InlineKeyboardButton('About ⛿', callback_data='about')
         ],[
             InlineKeyboardButton('Close 🔐', callback_data='close')
         ]]
